4759e30ac7138f4b6178f5e616f2292beb5c684f
Snowlly <evanmartin2001@gmail.com>
2025-12-04 13:48:39 +0100

    INIT : bdd

--- Model/BDD.php
+++ Model/BDD.php
@@ -2,7 +2,7 @@
 
 try {
     $password = "";
-    $bdd = new PDO('mysql:host=localhost;dbname=TrovaJob;charset=utf8;', 'root', $password, array(PDO::ATTR_PERSISTENT => true));
+    $bdd = new PDO('mysql:host=localhost;dbname=CRUDapp;charset=utf8;', 'root', $password, array(PDO::ATTR_PERSISTENT => true));
 } catch (PDOException $e) {
     die($e->getMessage());
 }
